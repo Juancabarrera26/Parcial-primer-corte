7a2f5014793e88b073bc2fbfcbe58070d595dbf2
ProyectCyber <108159807+Juancabarrera26@users.noreply.github.com>
2026-03-10 07:42:28 -0500

    Refine comments and correct example text
    Updated comments for clarity and fixed accentuation in the example text.

--- ejercicio1_expresion_regular.py
+++ ejercicio1_expresion_regular.py
@@ -1,18 +1,15 @@
-# Solución del ejercicio 1: Expresiones regulares
 
 import re
 
-# Definición de la expresión regular para buscar patrones
-# Ejemplo de patrón: buscar palabras que comienzan con una letra mayúscula
+# Definir la expresion regular para buscar los patrones
+# Ejemplo: buscar palabras que comienzan con una letra mayuscula
 patron = r'\b[A-Z][a-z]*\b'
 
 # Texto de ejemplo
-texto = "Este es un Texto de Ejemplo. Aquí hay algunas Palabras que comienzan con Mayúscula."
+texto = "Este es un Texto de Ejemplo. Aqui hay algunas Palabras que comienzan con Mayuscula."
 
-# Buscar todas las coincidencias en el texto
 coincidencias = re.findall(patron, texto)
 
-# Imprimir los resultados
 print('Palabras que comienzan con mayúscula:')
 for palabra in coincidencias:
     print(palabra)
